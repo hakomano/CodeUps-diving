6f605fc74ffcf99425b3a151ea4522d7c44a3d5d
hako <hakomano.with.me@gmail.com>
2024-03-26 18:54:37 +0900

    【共通】トップへ戻るボタン

--- src/js/script.js
+++ src/js/script.js
@@ -2,4 +2,69 @@
 jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
 
 
+
+  //========================================================*
+  // ページトップボタン処理(フッター手前で止まる)
+  //========================================================*
+  $(function (){
+    const pageTop = $(".js-page-top");
+
+    //トップへ戻るボタンのクリックイベント
+    // pageTop.on("click", scrollTop);
+    $(window).on("scroll", function() {
+      btnScrollEvent();
+      btnPosition();
+    });
+
+    //ページトップへスムーススクロールする
+    pageTop.click(function () {
+      $("body,html").animate(
+        {
+          scrollTop: 0,
+        },
+        500 //スクロールの速さを変える場合はここ
+      );
+      return false;
+    });
+
+    //700pxスクロールしたらボタンを表示・非表示
+    function btnScrollEvent() {
+      if ($(window).scrollTop() > 700) {
+        pageTop.fadeIn(); // 700px以上スクロールしたら表示
+      } else {
+        pageTop.fadeOut(); //スクロールが700px以下の場合、非表示
+      }
+    }
+
+    //フッター手前での処理(ボタンはフッターより上で止まる)
+    function btnPosition() {
+      const scrollHeight = $(document).height();
+      const scrollPosition = $(window).height() + $(window).scrollTop();
+      const footHeight = $("footer").outerHeight();
+
+      if (scrollHeight - scrollPosition <= footHeight && window.matchMedia("(max-width: 768px)").matches) {
+        pageTop.css({
+          position: "absolute",
+          bottom: footHeight + 16 + "px" //フッター手前のsp時
+        });
+      } else if (scrollHeight - scrollPosition <= footHeight && window.matchMedia("(min-width: 767px)").matches) {
+        pageTop.css({
+          position: "absolute",
+          bottom: footHeight + 20 + "px" //フッター手前のpc時
+        });
+      } else if(window.matchMedia("(max-width: 768px)").matches) {
+        pageTop.css({
+          position: "fixed",
+          bottom: 16 + "px" //sp時
+        });
+      } else if(window.matchMedia("(min-width: 767px)").matches) {
+        pageTop.css({
+          position: "fixed",
+          bottom: 20 + "px" //pc時
+        });
+      }
+    }
+  });
+
+
 });
