ceb1ba63b00f59f0f3f39490e60a4b4a70338c35
hako <hakomano.with.me@gmail.com>
2024-03-30 11:00:59 +0900

    【トップ】ピクセルパーフェクト調整

--- src/js/script.js
+++ src/js/script.js
@@ -158,7 +158,7 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
             spaceBetween: 30,
         },
         1230: {
-            slidesPerView: "3.5",
+            slidesPerView: "3.49",
             spaceBetween: 40,
         },
     },
