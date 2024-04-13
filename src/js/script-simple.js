cc25622ebab94f3728e66908f58ca4d7e64383b7
hako <hakomano.with.me@gmail.com>
2024-04-13 14:49:50 +0900

    【下層】404・利用規約・プライバシーポリシーページ

---
+++ src/js/script-simple.js
@@ -0,0 +1,126 @@
+
+jQuery(function ($) { // この中であればWordpressでも「$」が使用可能になる
+
+  //========================================================*
+  // ハンバーガーメニュー(フェードイン・アウト)
+  //========================================================*
+  $(function () {
+    $(".js-hamburger").on("click", function () {
+      $(this).toggleClass("is-open");
+      if ($(this).hasClass("is-open")) {
+        openDrawer();
+      } else {
+        closeDrawer();
+      }
+    });
+
+    // backgroundまたはページ内リンクをクリックで閉じる
+    $(".js-drawer a[href]").on("click", function () {
+      closeDrawer();
+    });
+
+    // resizeイベント
+    $(window).on('resize', function() {
+      if (window.matchMedia("(min-width: 768px)").matches) {
+        closeDrawer();
+      }
+    });
+  });
+
+  function openDrawer() {
+    $(".js-drawer").fadeIn();
+    $(".js-hamburger").addClass("is-open");
+  }
+
+  function closeDrawer() {
+    $(".js-drawer").fadeOut();
+    $(".js-hamburger").removeClass("is-open");
+  }
+
+  //========================================================*
+  // ページトップボタン処理(フッター手前で止まる)
+  //========================================================*
+  $(function (){
+    const pageTop = $(".js-page-top");
+
+    //トップへ戻るボタンのクリックイベント
+    // pageTop.on("click", scrollTop);
+    $(window).on("scroll", function() {
+      btnScrollEvent();
+      btnPosition();
+    });
+
+    //ページトップへスムーススクロールする
+    pageTop.click(function () {
+      $("body,html").animate(
+        {
+          scrollTop: 0,
+        },
+        500 //スクロールの速さを変える場合はここ
+      );
+      return false;
+    });
+
+    //400pxスクロールしたらボタンを表示・非表示
+    function btnScrollEvent() {
+      if ($(window).scrollTop() > 400) {
+        pageTop.fadeIn(); // 400px以上スクロールしたら表示
+      } else {
+        pageTop.fadeOut(); //スクロールが400px以下の場合、非表示
+      }
+    }
+
+    //フッター手前での処理(ボタンはフッターより上で止まる)
+    function btnPosition() {
+      const scrollHeight = $(document).height();
+      const scrollPosition = $(window).height() + $(window).scrollTop();
+      const footHeight = $("footer").outerHeight();
+
+      if (scrollHeight - scrollPosition <= footHeight && window.matchMedia("(max-width: 768px)").matches) {
+        pageTop.css({
+          position: "absolute",
+          bottom: footHeight + 16 + "px" //フッター手前のsp時
+        });
+      } else if (scrollHeight - scrollPosition <= footHeight && window.matchMedia("(min-width: 767px)").matches) {
+        pageTop.css({
+          position: "absolute",
+          bottom: footHeight + 20 + "px" //フッター手前のpc時
+        });
+      } else if(window.matchMedia("(max-width: 768px)").matches) {
+        pageTop.css({
+          position: "fixed",
+          bottom: 16 + "px" //sp時
+        });
+      } else if(window.matchMedia("(min-width: 767px)").matches) {
+        pageTop.css({
+          position: "fixed",
+          bottom: 20 + "px" //pc時
+        });
+      }
+    }
+  });
+
+  //========================================================*
+  // 画面サイズが変わったときに高さを更新する(100vh対応)
+  //========================================================*
+  window.addEventListener('resize', () => {
+    const vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  });
+
+  //========================================================*
+  // フッター表示されたら追従ヘッダーを非表示
+  //========================================================*
+  $(".footer").on('inview', function(event, isInViewHeader){
+    if (isInViewHeader) {
+      $(".js-header").fadeOut();
+    } else {
+      $(".js-header").fadeIn();
+    }
+  })
+
+
+
+
+});
+
